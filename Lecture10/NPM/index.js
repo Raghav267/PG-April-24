4e7ff65c60639794d947bf7fd9e7350a276daab8
Raghav267 <harshraghav089@gmail.com>
2024-06-29 08:23:59 +0530

    Npm packages

---
+++ Lecture10/NPM/index.js
@@ -0,0 +1,6 @@
+const giveMejoke = require('give-me-a-joke');
+const color = require('colors');
+
+// giveMejoke.getCustomJoke((joke)=>{console.log(joke)})
+
+giveMejoke.getRandomDadJoke((joke) => console.log(joke.rainbow.))
---
+++ Lecture10/PathModule/index.js
@@ -0,0 +1,6 @@
+const path = require('path');
+
+// console.log(path);
+const f1 = path.join(__dirname);
+console.log(f1);
+console.log(process.cwd());
